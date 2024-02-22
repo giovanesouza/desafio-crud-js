7196565873cae862abbd28143d9677e5384cd416
Giovane Souza <developergiovanesouza@gmail.com>
2024-02-22 01:04:13 -0300

    feat: CRUD completo - resolução 1

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 // Importação do readline para entrada de dados via teclado
 const input = require('readline-sync');
 
+// Declaração da lista
+let produtos = [];
 
-// To DO: Declare uma lista com nome 'produtos'
-
-
-
-let option; // Variável utilizada para armazenar a opção escolhida pelo usuário (utilize o readline-sync dentro do loop)
+ 
+let option; // Variável utilizada para armazenar a opção escolhida pelo usuário
 let id = 1; // id utilizado no cadastro de produtos
 
 
-
-// --- Esta função exibirá o MENU ---
+// Esta função exibirá o MENU
 function exibirMenu() {
     console.log("==========================");
     console.log("           CRUD           ");
@@ -27,140 +25,148 @@ function exibirMenu() {
 };
 
 
-// --- Funções do CRUD ---
+// Funções do CRUD: São as responsáveis pela manipulação dos dados
 
-// To DO: Implemente a função cadastrarProduto(id, nome, categoria, preco) com a sintaxe REGULAR
+function cadastrarProduto(id, nome, categoria, preco) {
+
+    // Monta objeto com os dados a serem salvos do produto
+    const productInfo = {
+        id,
+        nome,
+        categoria,
+        preco
+    };
+
+   produtos.push(productInfo); // Insere produto no array
+
+   return productInfo; // Retorna as informações do produto cadastrado
+};
 
 
-
-// To DO: Implemente a função listarProdutos() com a sintaxe REGULAR
-
-
-
-// To DO: Implemente a função buscarProdutoPorId(id) com a sintaxe REGULAR
+function listarProdutos() {
+    return produtos; // Retorna a lista com TODOS os produtos cadastrados
+};
 
 
+function buscarProdutoPorId(id) {
+    // Verifica se um produto existe com base no ID informado e retorna true ou false
+    const productFound = produtos.find(product => product.id === id);
+    
+    // Caso exista, exibe as informações do produto
+    if(productFound)
+        return productFound;
+    
+    // Caso não exista, retorna a mensagem abaixo
+    return "Produto não encontrado!";
+};
 
-// To DO: Altere a função abaixo para sintaxe de Arrow Function
-function atualizarProdutoPorId(id) {
-    // 1. Verifica se o produto existe
-    const productFound = buscarProdutoPorId(id);
 
-    /* 2. Se localizado, percorre o array para pegar o produto com o id informado, permitindo alterar seus dados
-    */
-    if (productFound) {
-        produtos.map((produto, index) => {
-            
+// Ao utilizar arrow function, quando houver APENAS UM parâmetro, os parênteses são opcionais.
+const atualizarProdutoPorId = id => {
+    const productFound = buscarProdutoPorId(id); // Antes de atualizar, verifica se o produto existe.
+    
+    // Se true, permite a sua atualização
+    if(productFound) {
+        produtos.map((product, index) => {
+        
             // Pega o elemento com base no ID passado, permitindo a alteração do produto certo
-            if (produto.id === id) {
-
+            if(product.id === id) {
+                
                 produtos[index].nome = input.question("Informe o novo nome do produto: ");
                 produtos[index].categoria = input.question("Informe a nova categoria do produto: ");
                 produtos[index].preco = input.questionFloat("Informe o novo preco do produto: ");
 
             };
-
         });
     };
-
-    // Se localizado, vai exibir as info. atualizadas, senão a mensagem configurada em buscarProdutoPorId(id).
+    
+    // Se localizado, vai exibir as info. atualizadas, senão mensagem configurada em buscarProdutoPorId(id).
     return productFound;
 };
 
 
-/*
- To DO: Implemente a função excluirProduto() na sintaxe de Arrow Function. Utilize o método .splice(index, 1) para remoção do produto e retorne o produto removido
-*/
+const excluirProdutoPorId = (id) => {
+    const productFound = buscarProdutoPorId(id);
+    
+    if(productFound) {
+        produtos.map((produto, index) => {
+            if(produto.id === id)
+                produtos.splice(index, 1); // Remove o produto.
+        });
+    }
+
+    // Se localizado, vai retornar as info. do produto excluído, senão mensagem configurada em buscarProdutoPorId(id).
+    return productFound;
+};
 
 
 
-
-// MENU
+// MENU: Aqui é a parte onde o usuário interage para realizar as funcionalidades do CRUD -> Criar, ler, atualizar e excluir produtos
 do {
-
     let productId; // Variável para pegar o ID do Produto.
 
-    // To DO: Chame a função para exibir o Menu
+    // Chama a função do Menu que exibirá as opções disponíveis
+    exibirMenu();
 
+    option = input.questionInt("Informe a opcao que deseja: ");
 
-    // To DO: Peça para o usuário informar uma opção - utilize os métodos do readline
-    option = ;
-
-
-    // Caso o usuário digite 0, sai do loop e encerra o programa
+    // Caso o usuário digite 0 => Sai do loop e encerra o programa
     if (option === 0)
         break;
 
-
-    // To DO: Informe abaixo o que será verificado no Switch    
-    switch () {
+    switch (option) {
         case 1:
-            console.clear();
-            console.log("-- Cadastrar produto --");
+            console.clear(); // Limpa o console
+            console.log("-- Cadastrar produto --"); // Exibe o título a opção selecionada
 
-            // To DO: Peça para o usuário inserir as informações do produto a ser cadastrado
-            let nome = ;
-            let categoria = ;
-            let preco = ;
+            // Entrada de dados: Necessário para realizar o cadastro de novos produtos
+            let nome = input.question("Informe o nome do produto: ");
+            let categoria = input.question("Informe a categoria produto: ");
+            let preco = input.questionFloat("Informe o preco do produto: ");
 
+            // Cadastra o produto e retorna as informações dele
+            console.log("Produto cadastrado: ", cadastrarProduto(id, nome, categoria, preco));
 
-            // To DO: Chame a função para cadastro de Produto e exiba seu retorno           
-
-
-            id++; // Realiza incremento no id
+            id++; // Incrementa 1 no ID
             break;
 
         case 2:
             console.clear();
-            console.log("-- Produtos cadastrados --");
+            console.log("-- Produtos cadastrados --"); 
 
-            // To DO: Chame a função para listagem de produtos - verifique se a lista está sendo impressa
-
-
+            console.log(listarProdutos()); // Exibe lista com TODOS os produtos cadastrados
             break;
 
         case 3:
             console.clear();
-            console.log("-- Buscar Produto por ID --");
+            console.log("-- Buscar Produto por ID --"); 
 
+            productId = input.questionInt("Informe o id do produto: ");
 
-            // To DO: Peça para o usuário informar o ID do produto que deseja buscar
-            productId = ;
-
-
-            // To DO: Chame a função de buscar produto e exiba o seu retorno
-
-
+            console.log("Informações do produto:", buscarProdutoPorId(productId));
             break;
 
         case 4:
             console.clear();
-            console.log("-- Atualizar produto -- ");
+            console.log("-- Atualizar produto -- "); 
 
-            // To DO: Peça para o usuário informar o ID do produto.
-            productId = ;
+            productId = input.questionInt("Informe o ID do produto: ");
 
-            // To Do: Chame a função de atualizar produto passando o ID informado pelo usuário como parâmetro - imprima o resultado
-
-
+            console.log("Produto atualizado: ", atualizarProdutoPorId(productId));
             break;
 
         case 5:
             console.clear();
+            console.log("-- Excluir produto -- "); 
 
-            // TO DO: Insira este código na função de remover produto
-            console.log("-- Excluir produto -- ");
-
-            // To DO: Peça para o usuário informar o ID do produto.
-            productId = ;
-
-            // To Do: Chame a função de excluir produto passando o ID informado como parâmetro - imprima o resultado
-
+            productId = input.questionInt("Informe o ID do produto: ");
+            
+            console.log("Produto excluído: ", excluirProdutoPorId(productId));
             break;
 
         default:
             console.clear();
-            console.log("Opção inválida! Informe um número válido.")
+            console.log("Opção inválida! Informe um número válido.");
             break;
     };
 
